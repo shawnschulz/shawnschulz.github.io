65e80ed29874a7c3cc24d12fafa32e210acd0bd3
shawnschulz <shawnschulz@gberkeley.edu>
2026-03-13 10:53:34 -0700

    Add the blog post test text file

--- src/page_switcher.js
+++ src/page_switcher.js
@@ -4,9 +4,33 @@ function showPage(id) {
   history.pushState({}, '', '#' + id);
 }
 
+// Uses the gridCell object to generate a page with grids
+function generateGridPage(gridCell) {
+}
+
+function generate_blog_post(post_name) {
+  var blogPage = document.getElementById("page-blog");
+  var text_element = document.createElement("p")
+  let fr = new FileReader();
+  let post_body =  fr.readAsText(post_name);
+  text_element.appendChild(post_body);
+  blogPage.appendChild(text_element);
+  const id = location.hash.replace('#', '') || "blog";
+  showPage(id)
+}
+
+function generate_post1() {
+  generate_blog_post("./docs/assets/blog_posts/post1.txt")
+}
+
+function generate_links() {
+}
+
+var prev_page_name = 'about'
+
 // Handle back/forward
 window.addEventListener('popstate', () => {
-  const id = location.hash.replace('#', '') || 'about';
+  const id = location.hash.replace('#', '') || prev_page_name;
   showPage(id);
 });
 
